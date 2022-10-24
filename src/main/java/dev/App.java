9814ebe8075ff8f79d9a7d6ca6f5c99a666cbf33
GuilletJoseph <guilletjoseph85@gmail.com>
2022-10-24 17:35:22 +0200

    ff

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,9 @@
 package dev;
 
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String asciiArt = FigletFont.convertOneLine("Hello World");
+        System.out.println(asciiArt);
     }
 }
